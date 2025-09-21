99bbbedc813cc2f8489c3f02f6c86d0e88fd193c
Dmitriy Ignatyev <dy.ignatyev@gmail.com>
2025-09-22 02:32:38 +0300

    init

--- Package.swift
+++ Package.swift
@@ -4,23 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftCollections+NonEmpty",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SwiftCollections+NonEmpty",
-            targets: ["SwiftCollections+NonEmpty"]
-        ),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SwiftCollections+NonEmpty"
-        ),
-        .testTarget(
-            name: "SwiftCollections+NonEmptyTests",
-            dependencies: ["SwiftCollections+NonEmpty"]
-        ),
-    ]
+  name: "SwiftCollections+NonEmpty",
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "SwiftCollections+NonEmpty",
+      targets: ["SwiftCollections+NonEmpty"],
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.2.1")),
+    .package(url: "https://github.com/iDmitriyy/swift-nonempty.git", .upToNextMajor(from: "0.5.0")),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "SwiftCollections+NonEmpty",
+    ),
+    .testTarget(
+      name: "SwiftCollections+NonEmptyTests",
+      dependencies: ["SwiftCollections+NonEmpty"],
+    ),
+  ],
 )
