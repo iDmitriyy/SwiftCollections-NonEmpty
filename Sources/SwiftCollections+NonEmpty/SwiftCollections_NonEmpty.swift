66d7815f12056e37da93946a7a3657a0559e38f6
Dmitriy Ignatyev <dy.ignatyev@gmail.com>
2025-09-22 03:04:14 +0300

    typealiases

--- Sources/SwiftCollections+NonEmpty/SwiftCollections_NonEmpty.swift
+++ Sources/SwiftCollections+NonEmpty/SwiftCollections_NonEmpty.swift
@@ -2,3 +2,15 @@
 // https://docs.swift.org/swift-book
 
 public typealias NonEmptyOrderedSet<T: Hashable> = NonEmpty<OrderedSet<T>>
+
+public typealias NonEmptyDeque<T: Hashable> = NonEmpty<Deque<T>>
+
+public typealias NonEmptyBitArray = NonEmpty<BitArray>
+
+public typealias NonEmptyBitSet = NonEmpty<BitSet>
+
+public typealias NonEmptyTreeDictionary<Key: Hashable, Value> = NonEmpty<TreeDictionary<Key, Value>>
+
+public typealias NonEmptyTreeSet<T: Hashable> = NonEmpty<TreeSet<T>>
+
+// public typealias NonEmptyOrderedDictionary<Key: Hashable, Value> = NonEmpty<OrderedDictionary<Key, Value>>
